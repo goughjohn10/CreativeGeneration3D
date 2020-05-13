e3623f1eda98dc146bcd3c4a9828964e6232d06d
goughjohn10 <goughjohn10@gmail.com>
2020-05-13 15:14:56 +0100

    Updated Code

---
+++ Creative 3D Generation/Assets/Scripts/PyramidFractal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramidFractal : MonoBehaviour
+{
+    public Mesh mesh;
+
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        new GameObject("PyramidChild").AddComponent<PyramidFractal>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
